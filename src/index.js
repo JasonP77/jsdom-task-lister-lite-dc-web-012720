9265f3e542e8183c053eac12e7b8d14af9bf54f9
JasonP77 <llvslr77@gmail.com>
2020-03-10 15:00:17 -0400

    Needs to work on stretch features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector(".form");
-  form.addEventListener('click', addFunction);
-});
+  form.addEventListener('submit', createTodo)}
+);
+;
 
-function addFunction() {
-  let input = document.querySelector(".input_field").value; 
-  console.log(input);
+function createTodo(e) {
+  e.preventDefault();
+  let ul = getUl();
+  let li = createLi();
+  ul.appendChild(li);
+  let input = document.querySelector(".input_field").value;
+  li.innerText = input;
 }
+
+function getUl(){
+  return document.querySelector('#tasks');
+}
+
+function createLi(){
+  let li = document.createElement("li");
+  return li
+}
+
