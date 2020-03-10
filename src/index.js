93b3abb86440bf6544c6204827373f192f587443
JasonP77 <llvslr77@gmail.com>
2020-03-10 14:21:33 -0400

    started

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector(".form");
+  form.addEventListener('click', addFunction);
 });
+
+function addFunction() {
+  let input = document.querySelector(".input_field").value; 
+  console.log(input);
+}
